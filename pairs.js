0c88da52b9a9cab6e58a6a057071cdcdb4771a49
Ahmad Helal <you@example.com>
2019-08-04 19:00:25 +0300

    ahmad & Nwaf

--- pairs.js
+++ pairs.js
@@ -1,44 +1,58 @@
 /**************************************************************
-* pairs(names):
-*
-* - It accepts an array of names:
-*       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish'])
-*
-* - It returns a randomized pairing of names:
-*       [['Mishmish', 'Asis'], ['Fawas', 'Hamsa']]
-*
-* - It can handle an odd number of names:
-*       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein'])
-*       returns [['Mishmish', 'Fawas'], ['Asis', 'Hussein'], ['Hamsa']]
-*
-* - It returns an empty array if it gets passed an empty array:
-*       pairs([]) returns []
-*
-* - It returns an empty array if it gets passed nothing:
-*       pairs() returns []
-****************************************************************/
+ * pairs(names):
+ *
+ * - It accepts an array of names:
+ *       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish'])
+ *
+ * - It returns a randomized pairing of names:
+ *       [['Mishmish', 'Asis'], ['Fawas', 'Hamsa']]
+ *
+ * - It can handle an odd number of names:
+ *       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein'])
+ *       returns [['Mishmish', 'Fawas'], ['Asis', 'Hussein'], ['Hamsa']]
+ *
+ * - It returns an empty array if it gets passed an empty array:
+ *       pairs([]) returns []
+ *
+ * - It returns an empty array if it gets passed nothing:
+ *       pairs() returns []
+ ****************************************************************/
+
 function pairs(names) {
-  // Your code goes here
+  let pairs = [];
+  pairs.push(names[0]);
+  for (let i = 1; i < names.length; i++) {
+    let random = names.getRandom();
+    let random2 = names.getRandom();
+
+    let y = [random2, random];
+
+    pairs.push(y);
+  }
+
+  return pairs;
 }
 
 module.exports = pairs;
 
 /**********************************************
-* READ ME!!!!
-*
-* We've included this handy method for you.
-* It will remove a random element from an array
-* and return it to you.
-*
-* Example Usage:
-*
-* let numbers = [1, 2, 3, 4];
-* let random = numbers.getRandom();  // randomly returns something from the array. e.g. 3
-* console.log(random); // 3 (the random element)
-* console.log(numbers);  // [1, 2, 4] (missing the random element)
-************************************************/
-Array.prototype.getRandom = function () {
-  return this.splice(Math.floor(Math.random()*this.length), 1)[0];
-}
+ * READ ME!!!!
+ *
+ * We've included this handy method for you.
+ * It will remove a random element from an array
+ * and return it to you.
+ *
+ * Example Usage:
+ *
+ * let numbers = [1, 2, 3, 4];
+ * let random = numbers.getRandom();  // randomly returns something from the array. e.g. 3
+ * console.log(random); // 3 (the random element)
+ * console.log(numbers);  // [1, 2, 4] (missing the random element)
+ ************************************************/
+Array.prototype.getRandom = function() {
+  return this.splice(Math.floor(Math.random() * this.length), 1)[0];
+};
 
-console.log(pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein', 'Lailz', 'Mr Potato']));
+console.log(
+  pairs(["Asis", "Hamsa", "Fawas", "Mishmish", "Hussein", "Lailz", "Mr Potato"])
+);
